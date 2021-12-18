97c1f1c139aa826423b68a64c42f989b3b07f2ff
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-18 17:03:09 +0200

    Add scaffoled routes for models

---
+++ test/controllers/reviews_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ReviewsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @review = reviews(:one)
+  end
+
+  test "should get index" do
+    get reviews_url, as: :json
+    assert_response :success
+  end
+
+  test "should create review" do
+    assert_difference('Review.count') do
+      post reviews_url, params: { review: { description: @review.description, laptop_id: @review.laptop_id, rating: @review.rating, title: @review.title, user_id: @review.user_id } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show review" do
+    get review_url(@review), as: :json
+    assert_response :success
+  end
+
+  test "should update review" do
+    patch review_url(@review), params: { review: { description: @review.description, laptop_id: @review.laptop_id, rating: @review.rating, title: @review.title, user_id: @review.user_id } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy review" do
+    assert_difference('Review.count', -1) do
+      delete review_url(@review), as: :json
+    end
+
+    assert_response 204
+  end
+end
