2e1e0e6d3e3b0f1de56173ae09f0cf81300e4665
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-18 15:59:33 +0200

    Create auth class to authorise request when received request with jwt token

---
+++ app/services/authorization_service.rb
@@ -0,0 +1,23 @@
+class AuthorizationService
+
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def authenticate_request!
+    verify_token
+  end
+
+  private
+
+  def http_token
+    if @headers['Authorization'].present?
+      @headers['Authorization'].split(' ').last
+    end
+  end
+
+  def verify_token
+    JsonWebToken.verify(http_token)
+  end
+
+end
