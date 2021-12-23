61062b4dc7527844a4099efc51fdb825392cb742
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-23 21:10:55 +0200

    Remove unused actions

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -16,28 +16,6 @@ class LaptopsController < SecuredController
     head :not_found
   end
 
-  # POST /laptops
-  def create
-    laptop = Laptop.create!(laptop_params)
-    render json: laptop, status: :created
-  end
-
-  # # PATCH/PUT /laptops/1
-  # def update
-  #   if @laptop.update(laptop_params)
-  #     render json: @laptop
-  #   else
-  #     render json: @laptop.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # DELETE /laptops/1
-  def destroy
-    laptop = Laptop.find(params[:id])
-    laptop.delete
-    head :no_content
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
